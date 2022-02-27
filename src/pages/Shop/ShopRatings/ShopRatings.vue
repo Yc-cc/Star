370f2537dc43176e8a1e0deec2ef42fdbf72d342
Yc-cc <2383723682@qq.com>
2022-02-27 18:25:26 +0800

    [feat]:商家评价详情

--- src/pages/Shop/ShopRatings/ShopRatings.vue
+++ src/pages/Shop/ShopRatings/ShopRatings.vue
@@ -22,12 +22,29 @@
       </div>
       <div class="split"></div>
       <div class="ratingselect">
-        <div class="rating-type border-1px"> <span class="block positive active"> 全部<span class="count">30</span> </span> <span class="block positive"> 满意<span class="count">28</span> </span> <span class="block negative"> 不满意<span class="count">2</span> </span> </div>
-        <div class="switch on"> <span class="iconfont icon-starmeituan"></span> <span class="text">只看有内容的评价</span> </div>
+        <div class="rating-type border-1px">
+          <span class="block positive" :class="{active:selectType === 2}" @click="setSelect(2)">
+            全部
+            <span class="count">{{ratings.length}}</span>
+            </span>
+          <span class="block positive" :class="{active:selectType === 0}" @click="setSelect(0)">
+            满意
+            <span class="count">{{positiveSize}}</span>
+            </span>
+          <span class="block negative" :class="{active:selectType === 1}" @click="setSelect(1)">
+            不满意
+            <span class="count">{{ratings.length - positiveSize}}</span>
+            </span>
+        </div>
+        <div class="switch" :class="{on:onlyShowText}" @click="toggleShowText">
+          <span class="iconfont icon-starmeituan"></span> <span class="text">只看有内容的评价</span>
+        </div>
       </div>
       <div class="rating-wrapper">
         <ul>
-          <li class="rating-item" v-for="(rating,index) in ratings" :key="index">
+          <li class="rating-item"
+              v-for="(rating,index) in fliterRatings"
+              :key="index">
             <div class="avatar"> <img width="28"
                    height="28"
                    :src="rating.avatar"> </div>
@@ -39,15 +56,18 @@
               </div>
               <p class="text">{{rating.text}}</p>
               <div class="recommend">
-                <span class="iconfont" :class="rating.rateType === 0? 'icon-stardianying':'icon-staryaoxiang'"></span>
-                <span class="item" v-for="(item,index) in rating.recommend" :key="index">{{item}}</span>
+                <span class="iconfont"
+                      :class="rating.rateType === 0? 'icon-stardianying':'icon-staryaoxiang'"></span>
+                <span class="item"
+                      v-for="(item,index) in rating.recommend"
+                      :key="index">{{item}}</span>
                 <!-- <span class="item">皮蛋瘦肉粥</span>
                 <span class="item">扁豆焖面</span> -->
               </div>
               <div class="time">{{rating.rateTime}}</div>
             </div>
           </li>
-          
+
         </ul>
       </div>
     </div>
@@ -57,248 +77,290 @@
 <script>
 import BScroll from 'better-scroll'
 import Star from '../../../components/Star/Star.vue'
-import {mapState} from 'vuex'
+import { mapState ,mapGetters} from 'vuex'
 export default {
-  mounted() {
-    this.$store.dispatch('getShopRatings',()=>{
-      this.$nextTick(()=>{
-        new BScroll(this.$refs.ratings,{
-          click:true
+
+  data () {
+    return {
+      onlyShowText:true, //是否只显示有文本内容
+      selectType:2  //选择评价类型 0满意1不满意2全部
+    }
+  },
+  mounted () {
+    this.$store.dispatch('getShopRatings', () => {
+      this.$nextTick(() => {
+        new BScroll(this.$refs.ratings, {
+          click: true
         })
       })
     })
   },
-  components:{
+  components: {
     Star
   },
-  computed:{
-    ...mapState(['info','ratings'])
+  methods: {
+    setSelect(type){
+      this.selectType = type
+    },
+    toggleShowText(){
+      this.onlyShowText = !this.onlyShowText
+    }
+  },
+  computed: {
+    ...mapState(['info', 'ratings']),
+    ...mapGetters(['positiveSize']),
+
+    
+    fliterRatings(){
+      //得到相关数据
+      const {ratings,onlyShowText,selectType} = this
+
+      //筛选过滤数据
+      return ratings.filter((rating)=>{
+        const {rateType,text} = rating
+        /**
+         * 条件1：
+         *    selectType 0/1/2
+         *    rateType 0/1
+         *    selectType === 2 || selectType === rateType
+         * 
+         * 条件2：
+         *    onlyShowText true/false
+         *    text true/false
+         *    !onlyShowText || text.length>0
+         */
+
+        return (selectType === 2 || selectType === rateType) && (!onlyShowText || text.length>0)
+      })
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .ratings{
-    position: absolute;
-    top: 198px;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    overflow: hidden;
-    background: #fff;
-    .overview{
-      display: flex;
-      padding: 18px 0;
-      .overview-left{
-        flex: 0 0 137px;
-        padding: 6px 0;
-        width: 137px;
-        border-right: 1px solid rgba(7, 17, 27, 0.1);
-        text-align: center;
-        @media only screen and (max-width: 320px){
-          flex: 0 0 120px;
-          width: 120px;
-        }
-        .score{
-          margin-bottom: 6px;
-          line-height: 28px;
-          font-size: 24px;
-          color: rgb(255,153,0);
-        }
-        .title{
-          margin-bottom: 8px;
-          line-height: 12px;
+.ratings {
+  position: absolute;
+  top: 198px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  background: #fff;
+  .overview {
+    display: flex;
+    padding: 18px 0;
+    .overview-left {
+      flex: 0 0 137px;
+      padding: 6px 0;
+      width: 137px;
+      border-right: 1px solid rgba(7, 17, 27, 0.1);
+      text-align: center;
+      @media only screen and (max-width: 320px) {
+        flex: 0 0 120px;
+        width: 120px;
+      }
+      .score {
+        margin-bottom: 6px;
+        line-height: 28px;
+        font-size: 24px;
+        color: rgb(255, 153, 0);
+      }
+      .title {
+        margin-bottom: 8px;
+        line-height: 12px;
+        font-size: 12px;
+        color: rgb(7, 17, 27);
+      }
+      .rank {
+        line-height: 10px;
+        font-size: 10px;
+        color: rgb(147, 153, 159);
+      }
+    }
+    .overview-right {
+      flex: 1;
+      padding: 6px 0 6px 24px;
+      @media only screen and (max-width: 320px) {
+        padding-left: 6px;
+      }
+      .score-wrapper {
+        margin-bottom: 8px;
+        font-size: 0;
+        .title {
+          display: inline-block;
+          line-height: 18px;
+          vertical-align: top;
           font-size: 12px;
           color: rgb(7, 17, 27);
         }
-        .rank{
-          line-height: 10px;
-          font-size: 10px;
-          color: rgb(147,153,159);
-        }
-      }
-      .overview-right{
-        flex: 1;
-        padding: 6px 0 6px 24px;
-        @media only screen and (max-width: 320px){
-          padding-left: 6px;
-        }
-        .score-wrapper{
-          margin-bottom: 8px;
-          font-size: 0;
-          .title{
-            display: inline-block;
-            line-height: 18px;
-            vertical-align: top;
-            font-size: 12px;
-            color: rgb(7, 17, 27);
-          }
-          .star{
-            display: inline-block;
-            margin: 0 12px;
-            vertical-align: top;
-          }
-          .score{
-            display: inline-block;
-            line-height: 18px;
-            vertical-align: top;
-            font-size: 12px;
-            color: rgb(255,153,0);
-          }
-        }
-        .delivery-wrapper{
-          font-size: 0;
-          .title{
-            line-height: 18px;
-            font-size: 12px;
-            color: rgb(7, 17, 27);
-            margin-left: 13px;
-          }
-          .delivery{
-            margin-left: 12px;
-            font-size: 12px;
-            color: rgb(147,153,159);
-          }
-        }
-      }
-    }
-    .split{
-      width: 100%;
-      height: 16px;
-      border-top: 1px solid rgba(7, 17, 27, 0.1);
-      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
-      background: #f3f5f7;
-    }
-    .ratingselect{
-      .rating-type{
-        padding: 18px 0;
-        margin: 0 18px;
-        // border-1px(rgba(7,17,27,0.1))
-        // border-bottom: 1px solid rgba(7, 17, 27, 0.1);
-        font-size: 0;
-        .block{
+        .star {
           display: inline-block;
-          padding: 8px 12px;
-          margin-right: 8px;
-          line-height: 16px;
-          border-radius: 1px;
-          font-size: 12px;
-          color: rgb(77,85,93);
-          background: rgba(77,85,93,0.2);
-          &.active{
-            background: rgb(241, 136, 79);
-            color: #fff;
-          }
-          .count{
-            margin-left: 2px;
-            font-size: 8px;
-          }
-        }
-      }
-      .switch{
-        padding: 12px 18px;
-        line-height: 24px;
-        border-bottom: 1px solid rgba(7, 17, 27, 0.1);
-        color: rgb(147,153,159);
-        font-size: 0;
-        &.on{
-          .icon-starmeituan{
-            color: rgb(241, 136, 79);
-          }
-        }
-        .icon-starmeituan{
-          display: inline-block;
+          margin: 0 12px;
           vertical-align: top;
-          margin-right: 4px;
-          font-size: 24px;
         }
-        .text{
+        .score {
           display: inline-block;
+          line-height: 18px;
           vertical-align: top;
           font-size: 12px;
+          color: rgb(255, 153, 0);
         }
       }
-    }
-    .rating-wrapper{
-      padding: 0 18px;
-      .rating-item{
-        display: flex;
-        padding: 18px 0;
-        // border-1px(rgba(7,17,27,0.1))
-        border-bottom: 1px solid rgba(7, 17, 27, 0.1);
-        .avatar{
-          flex: 0 0 28px;
-          width: 28px;
-          margin-right: 12px;
-          img{
-            border-radius: 50%;
-          }
+      .delivery-wrapper {
+        font-size: 0;
+        .title {
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(7, 17, 27);
+          margin-left: 13px;
         }
-        .content{
-          position: relative;
-          flex: 1;
-          .name{
-            margin-bottom: 4px;
-            line-height: 12px;
-            font-size: 10px;
-            color: rgb(7, 17, 27);
-          }
-          .star-wrapper{
-            margin-bottom: 6px;
-            font-size: 0;
-            .star{
-              display: inline-block;
-              margin-right: 6px;
-              vertical-align: top;
-            }
-            .delivery{
-              display: inline-block;
-              vertical-align: top;
-              line-height: 19px;
-              font-size: 14px;
-              color: rgb(147,153,159);
-            }
-          }
-          .text{
-            margin-bottom: 8px;
-            line-height: 18px;
-            color: rgb(7, 17, 27);
-            font-size: 12px;
-          }
-          .recommend{
-            line-height: 16px;
-            font-size: 0;
-            .icon-stardianying, .icon-staryaoxiang, .item{
-              display: inline-block;
-              margin: 0 8px 4px 0;
-              font-size: 13px;
-            }
-            .icon-stardianying{
-              color: rgb(241, 136, 79);
-              font-size: 20px;
-            }
-            .icon-staryaoxiang{
-              color: rgb(147,153,159);
-              font-size: 20px;
-            }
-            .item{
-              padding: 0 6px;
-              border: 1px solid rgba(7, 17, 27, 0.1);
-              border-radius: 1px;
-              color: rgb(147,153,159);
-              background: #fff;
-            }
-          }
-          .time{
-            position: absolute;
-            top: 0;
-            right: 0;
-            line-height: 12px;
-            font-size: 10px;
-            color: rgb(147,153,159);
-          }
+        .delivery {
+          margin-left: 12px;
+          font-size: 12px;
+          color: rgb(147, 153, 159);
         }
       }
     }
   }
+  .split {
+    width: 100%;
+    height: 16px;
+    border-top: 1px solid rgba(7, 17, 27, 0.1);
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+    background: #f3f5f7;
+  }
+  .ratingselect {
+    .rating-type {
+      padding: 18px 0;
+      margin: 0 18px;
+      // border-1px(rgba(7,17,27,0.1))
+      // border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+      font-size: 0;
+      .block {
+        display: inline-block;
+        padding: 8px 12px;
+        margin-right: 8px;
+        line-height: 16px;
+        border-radius: 1px;
+        font-size: 12px;
+        color: rgb(77, 85, 93);
+        background: rgba(77, 85, 93, 0.2);
+        &.active {
+          background: rgb(241, 136, 79);
+          color: #fff;
+        }
+        .count {
+          margin-left: 2px;
+          font-size: 8px;
+        }
+      }
+    }
+    .switch {
+      padding: 12px 18px;
+      line-height: 24px;
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+      color: rgb(147, 153, 159);
+      font-size: 0;
+      &.on {
+        .icon-starmeituan {
+          color: rgb(241, 136, 79);
+        }
+      }
+      .icon-starmeituan {
+        display: inline-block;
+        vertical-align: top;
+        margin-right: 4px;
+        font-size: 24px;
+      }
+      .text {
+        display: inline-block;
+        vertical-align: top;
+        font-size: 12px;
+      }
+    }
+  }
+  .rating-wrapper {
+    padding: 0 18px;
+    .rating-item {
+      display: flex;
+      padding: 18px 0;
+      // border-1px(rgba(7,17,27,0.1))
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+      .avatar {
+        flex: 0 0 28px;
+        width: 28px;
+        margin-right: 12px;
+        img {
+          border-radius: 50%;
+        }
+      }
+      .content {
+        position: relative;
+        flex: 1;
+        .name {
+          margin-bottom: 4px;
+          line-height: 12px;
+          font-size: 10px;
+          color: rgb(7, 17, 27);
+        }
+        .star-wrapper {
+          margin-bottom: 6px;
+          font-size: 0;
+          .star {
+            display: inline-block;
+            margin-right: 6px;
+            vertical-align: top;
+          }
+          .delivery {
+            display: inline-block;
+            vertical-align: top;
+            line-height: 19px;
+            font-size: 14px;
+            color: rgb(147, 153, 159);
+          }
+        }
+        .text {
+          margin-bottom: 8px;
+          line-height: 18px;
+          color: rgb(7, 17, 27);
+          font-size: 12px;
+        }
+        .recommend {
+          line-height: 16px;
+          font-size: 0;
+          .icon-stardianying,
+          .icon-staryaoxiang,
+          .item {
+            display: inline-block;
+            margin: 0 8px 4px 0;
+            font-size: 13px;
+          }
+          .icon-stardianying {
+            color: rgb(241, 136, 79);
+            font-size: 20px;
+          }
+          .icon-staryaoxiang {
+            color: rgb(147, 153, 159);
+            font-size: 20px;
+          }
+          .item {
+            padding: 0 6px;
+            border: 1px solid rgba(7, 17, 27, 0.1);
+            border-radius: 1px;
+            color: rgb(147, 153, 159);
+            background: #fff;
+          }
+        }
+        .time {
+          position: absolute;
+          top: 0;
+          right: 0;
+          line-height: 12px;
+          font-size: 10px;
+          color: rgb(147, 153, 159);
+        }
+      }
+    }
+  }
+}
 </style>
